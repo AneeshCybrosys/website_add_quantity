1efc9f5815001cb2ee0fa24f188b657bed389c76
AneeshCybrosys <aneeshmathapuzha@gmail.com>
2022-08-08 12:53:59 +0530

    commiting website

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Add Quantity",
+
+    'summary': """
+        Adds a button into the product template""",
+
+    'description': """
+        Description
+    """,
+
+    'author': "Minions 6",
+
+    'version': '15.0.1.0',
+    'depends': ['website_sale'],
+
+    'data': [
+        'views/add_quantity_button_template.xml',
+             ],
+}
